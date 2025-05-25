90b95617e623ce7f96bc71ccec4186157d97137f
myhodoo <winj0705@gmail.com>
2025-05-25 17:46:08 +0900

    이동처리(다음시간에계속)

--- Assets/Scripts/StoneAttack.cs
+++ Assets/Scripts/StoneAttack.cs
@@ -19,36 +19,46 @@ public class StoneAttack : MonoBehaviour
     //던지는 효과를 나타낼 때 scale을 커지고 작게 만들기
 
 
+    public bool m_ISAttack = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        Debug.Log($"확인 : {collision.name}, {collision.tag}");
         if (collision.gameObject.tag == "potion")
         {
             spriteRenderer.sprite = ChangeStone;
             //this.gameObject.layer = 7;
             
         }
-        if (collision.gameObject.tag == "Catapult")
-        {
-            this.transform.position = Stonehole.position;
-        }
 
-
-        if(this.transform.position == Stonehole.position)
+        if (m_ISAttack)
         {
             this.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-            
-            if(collision.gameObject.tag == "knife")
+
+            if (collision.gameObject.tag == "knife")
             {
                 //this.rb.isKinematic = false;
 
                 this.GetComponent<Collider2D>().isTrigger = true;
-                if (  this.transform.position == ArrivePos)
+                this.rb.velocity = Vector3.up * speed;
+                if (this.transform.position == ArrivePos)
                 {
                     Destroy(this.gameObject);
                 }
             }
 
+            m_ISAttack = false;
         }
+
+        if (collision.gameObject.tag == "Catapult")
+        {
+            this.transform.position = Stonehole.position;
+            m_ISAttack = true;
+        }
+
+        
+        
     }
 
 
