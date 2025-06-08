2ccf78d586117241b89281e52fc6e826dca86a16
myhodoo <winj0705@gmail.com>
2025-06-08 12:32:21 +0900

    적 프리랩 설정하고 2초에 한번씩 공격자리 생성

--- Assets/Scripts/TargetFollow.cs
+++ Assets/Scripts/TargetFollow.cs
@@ -1,10 +1,11 @@
 using System.Collections;
+using UnityEditor.ShaderGraph.Serialization;
 using UnityEngine;
 
 public class TargetFollow : MonoBehaviour
 {
     
-    public Transform targetpos;
+    public PlayerMove targetpos;
     public float MoveSpeed = 0.3f;
     public GameObject cursor;
     public float time = 0;
@@ -12,38 +13,80 @@ public class TargetFollow : MonoBehaviour
 
     void Start()
     {
-        
-        StartCoroutine(time_increase());
+        targetpos = GameObject.FindAnyObjectByType<PlayerMove>();
+        StartCoroutine( time_increase() );
 
+        StartCoroutine(CursorFollow_Coroutine());
     }
 
+
+    bool m_ISFollow = false;
     IEnumerator time_increase()
     {
+        m_ISFollow = true;
+        yield return new WaitForSecondsRealtime(0.01f); // 낙하지점이 따라오는 속도와 멈추는 값(0.01초 후 그 지점에서 낙하)
+
+        m_ISFollow = false;
+
+
+        //while (true)
+        //{
+        //    time++;
+        //    yield return new WaitForSecondsRealtime(1f);    
+        //}
+        
+    }
+
+    IEnumerator CursorFollow_Coroutine()
+    {
+        cursor.SetActive(true);
+
+        while (m_ISFollow)
+        {
+            cursor.transform.position = Vector2.MoveTowards(cursor.transform.position, targetpos.transform.position, MoveSpeed * Time.deltaTime);
+
+            yield return null;
+        }
+
+
+        // 돌덩어리 떨어지도록 만들기
         while(true)
         {
-            time++;
-            yield return new WaitForSecondsRealtime(1f);    
+
+            yield return null;
+
+            // 예제코드
+            yield return new WaitForSecondsRealtime(2f); //낙하지점이 바닥에 닿아서 유지되는 시간
+
+            if (true)
+            {
+                break;
+            }
         }
-        
-    }
 
-
-    void CursorFollow()
-    {
+        //yield return new WaitForSecondsRealtime(2.5f);
         cursor.SetActive(false);
-        cursor.transform.position = Vector2.MoveTowards(cursor.transform.position, targetpos.position, MoveSpeed * Time.deltaTime);
+
+        yield return new WaitForSecondsRealtime(1f);
+        GameObject.Destroy(gameObject);
+    }
+
+    //void CursorFollow()
+    //{
+    //    cursor.SetActive(false);
+    //    cursor.transform.position = Vector2.MoveTowards(cursor.transform.position, targetpos.transform.position, MoveSpeed * Time.deltaTime);
         
 
-        if(time % 3 == 0)
-        {
-            cursor.SetActive(true);
-            cursor.transform.position = targetpos.position;
-        }
-    }
+    //    if(time % 3 == 0)
+    //    {
+    //        cursor.SetActive(true);
+    //        cursor.transform.position = targetpos.transform.position;
+    //    }
+    //}
 
-    void Update()
-    {
-        CursorFollow();
+    //void Update()
+    //{
+    //    CursorFollow();
 
-    }
+    //}
 }
