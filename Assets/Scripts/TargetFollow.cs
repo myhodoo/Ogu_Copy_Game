398bb56e968e9eccf85de36cb3a5473c13ff5e69
myhodoo <winj0705@gmail.com>
2025-06-08 11:16:37 +0900

    낙하 지점이 3초 간격으로 보이게 하기

--- Assets/Scripts/TargetFollow.cs
+++ Assets/Scripts/TargetFollow.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class TargetFollow : MonoBehaviour
@@ -5,21 +6,44 @@ public class TargetFollow : MonoBehaviour
     
     public Transform targetpos;
     public float MoveSpeed = 0.3f;
-    public Transform cursorpos;
+    public GameObject cursor;
+    public float time = 0;
+
 
     void Start()
     {
         
+        StartCoroutine(time_increase());
+
+    }
+
+    IEnumerator time_increase()
+    {
+        while(true)
+        {
+            time++;
+            yield return new WaitForSecondsRealtime(1f);    
+        }
         
     }
 
+
     void CursorFollow()
     {
-        cursorpos.position = Vector2.MoveTowards(cursorpos.position, targetpos.position, MoveSpeed * Time.deltaTime);
+        cursor.SetActive(false);
+        cursor.transform.position = Vector2.MoveTowards(cursor.transform.position, targetpos.position, MoveSpeed * Time.deltaTime);
+        
+
+        if(time % 3 == 0)
+        {
+            cursor.SetActive(true);
+            cursor.transform.position = targetpos.position;
+        }
     }
 
     void Update()
     {
         CursorFollow();
+
     }
 }
